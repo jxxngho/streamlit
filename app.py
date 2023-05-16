1d2a102939cf36ce757329599a5ec52238849a2c
jxxngho <109848417+jxxngho@users.noreply.github.com>
2023-05-16 12:47:43 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 st.title("Map")
-df = pd.DataFrame(np.random.randn(500,2)/[50,50]+[37.76 - 122.4],colimns['lat','lon'])
+df = pd.DataFrame(np.random.randn(500,2)/[50,50]+[37.76 - 122.4],columns['lat','lon'])
 st.map(df)
 
-x = st.slider('Selcet a value')
-st.write(x, 'squared is', x*x)
+# x = st.slider('Selcet a value')
+# st.write(x, 'squared is', x*x)
+
+rand = np.random.normal(1, 2, size=20)
+fig, ax = plt.subplots()
+ax.hist(rand, bins = 15)
+st.pyplot(fig)
