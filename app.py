871fd4ef1e31fbd5e3ecd68ceb9b6c09681eaf54
jxxngho <109848417+jxxngho@users.noreply.github.com>
2023-06-06 16:44:19 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ st.title("Map")
 df = pd.DataFrame(np.random.randn(500,2)/[50,50]+[30,30]+[37.76,-122.4],columns=['lat','lon'])
 st.map(df)
 
-# x = st.slider('Selcet a value')
-# st.write(x, 'squared is', x*x)
+x = st.slider('Selcet a value')
+st.write(x, 'squared is', x*x)
 
 rand = np.random.normal(1, 2, size=20)
 fig, ax = plt.subplots()
